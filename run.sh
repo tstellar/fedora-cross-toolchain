db5d96a6396bf911af10e55c7810b46934e63f1b
Tom Stellard <tstellar@redhat.com>
2023-03-15 21:26:04 -0700

    Working with new system

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 set -ex
 
+# Setup container images.  Create separate
+
 # Create builder containers
 for arch in ppc64le s390x; do #ppc64le s390x aarch64; do
     mkdir -p tree/$arch/
@@ -9,3 +11,5 @@ done
 podman pull --arch amd64 registry.fedoraproject.org/fedora:37
 
 podman build  -f Dockerfile.x86_64 -t cross-toolchain:f37
+
+podman build -f Dockerfile.test -t cross-toolchain-test:f37
