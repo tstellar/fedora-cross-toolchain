7d0bc375c8f20bc62c0fca2ce00622579477effd
Tom Stellard <tstellar@redhat.com>
2023-03-17 01:10:08 -0700

    Parameterize the Dockerfiles better

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
 set -ex
 
 for arch in aarch64 ppc64le; do
-    podman build --build-arg TARGET_ARCH=$arch --build-arg HOST_ARCH=x86_64 -f Dockerfile.cross -t $arch-redhat-linux-clang:37
-    podman build --build-arg TARGET_ARCH=$arch -f Dockerfile.test
+    os_name=fedora
+    os_version=37
+    host_container=quay.io/fedora/fedora:37-x86_64
+    target_container=quay.io/fedora/fedora:37-$arch
+    container_name=$os_name/$arch-redhat-linux-clang:$os_version
+    podman build --build-arg HOST_CONTAINER=$host_container \
+                 --build-arg TARGET_CONTAINER=$target_container \
+                 -f Dockerfile.cross -t $container_name
+    podman build --build-arg TARGET_CONTAINER=$target_container \
+                 --build-arg HOST_CONTAINER=$container_name \
+                 --build-arg TARGET_ARCH=$arch \
+                  -f Dockerfile.test
 done
