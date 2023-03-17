7f822bf421f6b3d9dd2180dfd17b6d2ba98c8ad2
Tom Stellard <tstellar@redhat.com>
2023-03-16 20:16:57 -0700

    Drop s390x builds due to lack of lld

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 set -ex
 
-for arch in aarch64 ppc64le s390x; do #ppc64le s390x aarch64; do
+for arch in aarch64 ppc64le; do
     podman build --build-arg TARGET_ARCH=$arch --build-arg HOST_ARCH=x86_64 -f Dockerfile.cross -t $arch-redhat-linux-clang:37
     podman build --build-arg TARGET_ARCH=$arch -f Dockerfile.test
 done
